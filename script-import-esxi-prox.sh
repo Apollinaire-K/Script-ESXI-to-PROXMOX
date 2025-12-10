033c10bbcbd0e70d3404825896cca11ea23f89de
Apollinaire-K <mathis.mivrozch@gmail.com>
2025-12-10 15:32:05 +0100

    Refine VM_NAME extraction in script-import-esxi-prox.sh
    Updated VM_NAME extraction to remove asterisks and ensure cleaner output.

--- script-import-esxi-prox.sh
+++ script-import-esxi-prox.sh
@@ -15,7 +15,7 @@ RAM=$(grep memSize "$path_to_vm".vmx | awk '{print $3}' | tr -d '"') # here we d
 NUM_CPU_TOTAL=$(grep numvcpus "$path_to_vm".vmx | awk '{print $3}' | tr -d '"')
 CORES=$(grep cpuid.coresPerSocket "path_to_vm".vmx | sed -n '1p' | awk '{print $3}' | tr -d '"') # Here we will have to had "sed -n '1p'" because the grep give us the result of cpuid.coresPerSocket & the one from "cpuid.coresPerSocket.cookie" so we use it to only get the first line.
 SOCKET=$(( NUM_CPU_TOTAL / CORES )) # to get the number of socket we divide the number total of core per number of core for one socket.
-VM_NAME=$(grep displayName "path_to_vm".vmx | awk '{ $1=" "; $2=" "; sub(/^ +/, " ");print}' | tr -d '"'| tr -d ' ')
+VM_NAME=$(grep displayName "path_to_vm".vmx | awk '{ $1=" "; $2=" "; sub(/^ +/, " ");print}' | tr -d '"'| tr -d ' ' | tr -d '*') # Updated to make sure all sign like " " or "*" are removed by the tr
 
 # Here this line of code will make sure that in case of the SOCKET is set at 0 it reset it to at least 1,
 [ "$SOCKET" -eq 0 ] && SOCKET =1
